292d288211f45c329a6adea73ca4ba3b80707371
Micka <mickael.murmann@gmail.com>
2024-02-28 07:38:19 +0100

    color status, progress and format ticket date fr-FR

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -4,7 +4,24 @@ import ProgressDisplay from "./ProgressDisplay"
 import StatusDisplay from "./StatusDisplay"
 
 const TicketCard = ({ticket}) => {
-  return (
+    
+    const formatTimestamp = (timestamp) => {
+        const options = {
+            year: "numeric",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: false,
+        }
+
+        const date = new Date(timestamp)
+        const formattedDate = date.toLocaleString("fr-FR", options);
+
+        return formattedDate;
+    }
+  
+    return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
         <div className="flex mb-3">
             <PriorityDisplay priority={ticket.priority} />
@@ -12,19 +29,23 @@ const TicketCard = ({ticket}) => {
                 <DeleteBlock />
             </div>
         </div>
-        <h4>Ticket Title</h4>
+        <h4>{ticket.title}</h4>
         <hr className="h-px border-0 bg-page mb-2"/>
         <p className="whitespace-pre-wrap">
-            this is the ticket description
+            {ticket.description}
         </p>
         <div className="flex-grow"></div>
         <div className="flex mt-2">
             <div className="flex flex-col">
-                <p className="text-xs my-1">01/01/2024 10:00PM</p>
-                <ProgressDisplay />
+                <p className="text-xs my-1">{formatTimestamp(ticket.createdAt)}</p>
+                <ProgressDisplay 
+                    progress={ticket.progress}
+                />
             </div>
             <div className="ml-auto flex items-end">
-                <StatusDisplay />
+                <StatusDisplay 
+                    status={ticket.status}
+                />
             </div>
         </div>
     </div>
