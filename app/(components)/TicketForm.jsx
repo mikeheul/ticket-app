16fcb9aed19b046de94245472fe17373feffdc90
Micka <mickael.murmann@gmail.com>
2024-03-01 14:47:53 +0100

    invert push and refresh method to see effective update

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -43,8 +43,8 @@ const TicketForm = ({ticket}) => {
             }
         }
 
-        router.refresh();
         router.push("/");
+        router.refresh();
     };
 
     const startingTicketData = {
